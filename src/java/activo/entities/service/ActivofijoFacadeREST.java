2b5f75087369ca3d3dac2c0362fc0aee5b8e9847
Mazf <Mazf@Karen-PC>
2017-05-08 21:38:13 -0500

    Adicion salida json.

--- src/java/activo/entities/service/ActivofijoFacadeREST.java
+++ src/java/activo/entities/service/ActivofijoFacadeREST.java
@@ -13,6 +13,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Clase con la implementacion de metodos a nivel de base de datos.
@@ -30,14 +31,14 @@ public class ActivofijoFacadeREST extends AbstractFacade<Activofijo> {
 
     @POST
     @Override
-    @Consumes({"application/xml", "application/json"})
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public void create(Activofijo entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({"application/xml", "application/json"})
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public void edit(@PathParam("id") Integer id, Activofijo entity) {
         super.edit(entity);
     }
@@ -50,14 +51,14 @@ public class ActivofijoFacadeREST extends AbstractFacade<Activofijo> {
 
     @GET
     @Path("{id}")
-    @Produces({"application/xml", "application/json"})
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public Activofijo find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({"application/xml", "application/json"})
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public List<Activofijo> findAll() {
         return super.findAll();
     }
