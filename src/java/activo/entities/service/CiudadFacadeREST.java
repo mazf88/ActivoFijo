17acf5c3719b84a696f92735b7c34fdafc65766d
Mazf <Mazf@Karen-PC>
2017-05-08 21:38:55 -0500

    Adicion salida json.

--- src/java/activo/entities/service/CiudadFacadeREST.java
+++ src/java/activo/entities/service/CiudadFacadeREST.java
@@ -13,6 +13,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Clase con la implementacion de metodos a nivel de base de datos.
@@ -30,14 +31,14 @@ public class CiudadFacadeREST extends AbstractFacade<Ciudad> {
 
     @POST
     @Override
-    @Consumes({"application/xml", "application/json"})
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public void create(Ciudad entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({"application/xml", "application/json"})
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public void edit(@PathParam("id") Integer id, Ciudad entity) {
         super.edit(entity);
     }
@@ -50,14 +51,14 @@ public class CiudadFacadeREST extends AbstractFacade<Ciudad> {
 
     @GET
     @Path("{id}")
-    @Produces({"application/xml", "application/json"})
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public Ciudad find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({"application/xml", "application/json"})
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public List<Ciudad> findAll() {
         return super.findAll();
     }
